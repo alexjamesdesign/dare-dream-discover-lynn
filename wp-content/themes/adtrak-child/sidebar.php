669e4c25cf134408ef7b3c2f1443d63124181265
Alex James <48618516+alexjamesdesign@users.noreply.github.com>
2020-03-25 16:54:28 +0000

    Testing out a remarketing ad

--- wp-content/themes/adtrak-child/sidebar.php
+++ wp-content/themes/adtrak-child/sidebar.php
@@ -23,6 +23,7 @@ if (is_home() || is_singular('destinations') || is_month() || is_category() ||  
 				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 			</div>
 			<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+			
 		<?php endif; ?>
 	</div>
 
@@ -44,6 +45,9 @@ if (is_home() || is_singular('destinations') || is_month() || is_category() ||  
 		<?php endforeach; ?>
 
 	</div>
+
+	<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=2&p=12&l=ez&f=ifr&linkID=2a64973e4a7a8f3e070c45e97776e908&t=daredreamdisc-21&tracking_id=daredreamdisc-21" width="100%" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+
 
 <?php else: ?>
 
