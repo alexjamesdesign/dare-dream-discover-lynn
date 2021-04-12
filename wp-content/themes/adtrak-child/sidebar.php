629a6eb2abcb6996723dd5af06e723b740370734
Alex James <hello@alexjamesdesign.co.uk>
2021-04-12 17:14:37 +0100

    Added Amazon ad to sidebar

--- wp-content/themes/adtrak-child/sidebar.php
+++ wp-content/themes/adtrak-child/sidebar.php
@@ -46,10 +46,16 @@ if (is_home() || is_singular('destinations') || is_month() || is_category() ||  
 
 	</div>
 
-	<div id="adgshp-1436804948"></div>
-	<script type="text/javascript" src="//cdn0.agoda.net/images/sherpa/js/init-dynamic_v8.min.js"></script><script type="text/javascript">
-		var stg = new Object(); stg.crt="15891159522699";stg.version="1.05"; stg.id=stg.name="adgshp-1436804948"; stg.Width="300px"; stg.Height="300px";stg.RefKey="kF7efl5tYjS4/gdBEvep9g==";stg.AutoScrollSpeed=3000;stg.AutoScrollToggle=true;stg.SearchboxShow=false;stg.DiscountedOnly=false;stg.Layout="squaredynamic"; stg.Language="en-us";stg.ApiKey="";stg.Cid="1845671";  stg.City="16808";stg.Currency="GBP";stg.OverideConf=false; new AgdDynamic('adgshp-1436804948').initialize(stg);
-	</script>
+	<div class="sidebar-item">
+
+		<div id="adgshp-1436804948"></div>
+		<script type="text/javascript" src="//cdn0.agoda.net/images/sherpa/js/init-dynamic_v8.min.js"></script><script type="text/javascript">
+			var stg = new Object(); stg.crt="15891159522699";stg.version="1.05"; stg.id=stg.name="adgshp-1436804948"; stg.Width="300px"; stg.Height="300px";stg.RefKey="kF7efl5tYjS4/gdBEvep9g==";stg.AutoScrollSpeed=3000;stg.AutoScrollToggle=true;stg.SearchboxShow=false;stg.DiscountedOnly=false;stg.Layout="squaredynamic"; stg.Language="en-us";stg.ApiKey="";stg.Cid="1845671";  stg.City="16808";stg.Currency="GBP";stg.OverideConf=false; new AgdDynamic('adgshp-1436804948').initialize(stg);
+		</script>
+
+		<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=2&p=12&l=ez&f=ifr&linkID=2a64973e4a7a8f3e070c45e97776e908&t=daredreamdisc-21&tracking_id=daredreamdisc-21" width="300" height="250" style="margin-top:10px" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+	
+	</div>
 
 
 <?php else: ?>
